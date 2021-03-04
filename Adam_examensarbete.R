f10bead7f364f97fbb40eddcee834567ca06c669
Adam Lofquist <adam.lvkvist@gmail.com>
2021-03-04 12:10:59 +0100

    adam correlation

--- Adam_examensarbete.R
+++ Adam_examensarbete.R
@@ -9,7 +9,6 @@ subset <- data.frame(p = unlist(data$p_avg[1:10000]),
                      date = unlist(data$trd_exctn_dt[1:10000]),
                      cusip = unlist(data$ï..cusip_id[1:10000]))
 
-ggplot(data = subset, aes(x=date, y=p, color=cusip))+
 ggplot(data = subset, aes(x=date, y=p, color=cusip)) +
     geom_line(data = subset, size = 0.5)
 
