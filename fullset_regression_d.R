86fb629a917c7bc8701cfb3a25447103cbf0686a
bigmanOtto <erikottossonrlive.se>
2021-04-06 18:42:49 +0200

    resid

--- fullset_regression_d.R
+++ fullset_regression_d.R
@@ -11,13 +11,6 @@ names(data)[1] <- paste("cusip_id")
 data$trd_exctn_dt <- as.Date.character(data$trd_exctn_dt, format = "%Y%m%d")
 is_significant <- 0.05
 
-##d~vol_tot
-model.1 <- lm(d ~ vol_tot, data=data)
-
-model.1.data <- data.frame(coef = coefficients(model.1),
-                           conf = confint(model.1),
-                           p_value = summary(model.1)$coef[2],
-                           r_squared = summary(model.1)$r.squared)
 
 ##d~vol_tot
 model.1 <- lm(d ~ vol_tot, data=data)
@@ -26,6 +19,10 @@ model.1.data <- data.frame(coef = coefficients(model.1),
                            conf = confint(model.1),
                            p_value = summary(model.1)$coef[2,4],
                            r_squared = summary(model.1)$r.squared)
+res.1<-residuals(model.1)
+hist(res.1)
+qqnorm(res.1)
+qqline(res.1)
 ##d~p_avg
 model.2 <- lm(d ~ p_avg, data=data)
 
@@ -33,7 +30,10 @@ model.2.data <- data.frame(coef = coefficients(model.2),
                            conf = confint(model.2),
                            p_value = summary(model.2)$coef[2,4],
                            r_squared = summary(model.2)$r.squared)
-
+res.2<-residuals(model.2)
+hist(res.2)
+qqnorm(res.2)
+qqline(res.2)
 ##d~spread
 model.3 <- lm(d ~ spread1, data=data)
 
@@ -41,7 +41,10 @@ model.3.data <- data.frame(coef = coefficients(model.3),
                            conf = confint(model.3),
                            p_value = summary(model.3)$coef[2,4],
                            r_squared = summary(model.3)$r.squared)
-
+res.3<-residuals(model.3)
+hist(res.3)
+qqnorm(res.3)
+qqline(res.3)
 
 ##d~spread+vol_tot
 model.4 <- lm(d ~ spread1+vol_tot, data=data)
@@ -50,6 +53,10 @@ model.4.data <- data.frame(coef = coefficients(model.4),
                            conf = confint(model.4),
                            p_value = summary(model.4)$coef[2,4],
                            r_squared = summary(model.4)$r.squared)
+res.4<-residuals(model.4)
+hist(res.4)
+qqnorm(res.4)
+qqline(res.4)
 
 ##d~spread+log(vol_tot)
 model.5 <- lm(d ~ spread1+log(vol_tot), data=data)
@@ -58,6 +65,10 @@ model.5.data <- data.frame(coef = coefficients(model.5),
                            conf = confint(model.5),
                            p_value = summary(model.5)$coef[2,4],
                            r_squared = summary(model.5)$r.squared)
+res.5<-residuals(model.5)
+hist(res.5)
+qqnorm(res.5)
+qqline(res.5)
 
 ##d~spread+sqrt(vol_tot)
 model.6 <- lm(d ~ spread1+sqrt(vol_tot), data=data)
@@ -66,6 +77,11 @@ model.6.data <- data.frame(coef = coefficients(model.6),
                            conf = confint(model.6),
                            p_value = summary(model.6)$coef[2,4],
                            r_squared = summary(model.6)$r.squared)
+res.6<-residuals(model.6)
+hist(res.6)
+qqnorm(res.6)
+qqline(res.6)
+
 
 ##d~rating
 model.7 <- lm(d ~ credit, data=data)
@@ -74,6 +90,11 @@ model.7.data <- data.frame(coef = coefficients(model.7),
                            conf = confint(model.7),
                            p_value = summary(model.7)$coef[2,4],
                            r_squared = summary(model.7)$r.squared)
+res.7<-residuals(model.7)
+hist(res.7)
+qqnorm(res.7)
+qqline(res.7)
+
 ##d~rating+sqrt(vol_tot)+spread
 model.8 <- lm(d ~ credit+sqrt(vol_tot)+spread, data=data)
 
@@ -82,6 +103,11 @@ model.8.data <- data.frame(coef = coefficients(model.8),
                            p_value = summary(model.8)$coef[2,4],
                            r_squared = summary(model.8)$r.squared)
 
+res.8<-residuals(model.8)
+hist(res.8)
+qqnorm(res.8)
+qqline(res.8)
+
 ##d~rating+log(vol_tot)+spread
 model.9 <- lm(d ~ credit+log(vol_tot)+spread, data=data)
 
@@ -89,4 +115,8 @@ model.9.data <- data.frame(coef = coefficients(model.9),
                            conf = confint(model.9),
                            p_value = summary(model.9)$coef[2,4],
                            r_squared = summary(model.9)$r.squared)
+res.9<-residuals(model.9)
+hist(res.9)
+qqnorm(res.9)
+qqline(res.9)
 
